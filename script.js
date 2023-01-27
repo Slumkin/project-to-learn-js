dfb866ebf82bc0ead95a21e595a359ac58d85828
kamila <charmed.kami97@gmail.com>
2023-01-27 12:24:21 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// alert('hello');
+
+// const result = confirm("Are you pidr?");
+// console.log(result);
+
+// const answer = prompt("Are you 18?", "");
+// console.log(answer);
+
+// const answers = [];
+
+// answers[0] = prompt("Whats your name?", '');
+// answers[1] = prompt("Whats your surname?", "");
+// answers[2] = prompt("How old are you?", "");
+
+// document.write(answers);
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Kamila";
+// alert(`Hello, ${user}`);
+
+ 
